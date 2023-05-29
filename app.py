17a1355c03a484c789ff59b2e8b91bb5af37eab2
Franklin_Willemen <fjfwtjw@protonmail.com>
2023-05-29 18:00:15 +0200

    Dutch

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ openai.api_key = os.environ['OPEN_AI_KEY']
 
 speech_config = speechsdk.SpeechConfig(subscription=os.environ['AZURE_SPEECH_KEY'], region="westeurope")
 #audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"
+speech_config.speech_synthesis_voice_name = "nl-NL-ColetteNeural"
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
 
 messages = [{"role": "system", "content": 'You are a clever and helpfull conversationalist. Respond to all input in 25 words or less. Do not mention that you are an AI or service of any kind.'}]
