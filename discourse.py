6f6ee84a7b3fcab2c33e9a0bd570350f7a42e4e6
Franklin_Willemen <fjfwtjw@protonmail.com>
2023-06-02 23:50:51 +0200

    attempt at memory

--- discourse.py
+++ discourse.py
@@ -2,6 +2,18 @@ import openai
 import os
 import azure.cognitiveservices.speech as speechsdk
 
+import csv
+from huggingface_hub import Repository
+
+DATASET_REPO_URL = "https://huggingface.co/datasets/FranklinWillemen/persistent-space-dataset"
+DATA_FILENAME = "data.txt"
+DATA_FILE = os.path.join("data", DATA_FILENAME)
+HF_TOKEN = os.environ.get("HF_TOKEN")
+
+repo = Repository(
+    local_dir="data", clone_from=DATASET_REPO_URL, use_auth_token=HF_TOKEN
+)
+
 openai.api_key = os.environ['OPEN_AI_KEY']
 
 speech_config = speechsdk.SpeechConfig(subscription=os.environ['AZURE_SPEECH_KEY'], region="westeurope")
@@ -26,16 +38,17 @@ def gen_voice(response, response_filename):
     stream = speechsdk.AudioDataStream(reponse_audio)
     stream.save_to_wav_file(response_filename)
 
-def save_context():
-    chat_transcript = ""
-    file_name = "context.txt"
+def memory(context:list):
 
-    with open(file_name, 'w') as file:
+    with open(DATA_FILE, "a") as csvfile:
         for message in context:
-            if message['role'] != 'system':
-                chat_transcript = message['role'] + ": " + message['content'] + "\n\n"
-                file.write(chat_transcript)
-
+            writer = csv.DictWriter(csvfile, fieldnames=["name", "message"])
+            writer.writerow(
+                {"name": message['role'], "message": message['content']}
+            )
+        commit_url = repo.push_to_hub()
+        print(commit_url)
+    
 def respond(audio:str):
     transcript = transcribe("whisper-1", audio)
     context.append({"role": "user", "content": transcript['text']})
