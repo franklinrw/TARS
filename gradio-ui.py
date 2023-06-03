f43a6b07fcde4d4e4a9cf6ccfaab4e7d51573131
Franklin_Willemen <fjfwtjw@protonmail.com>
2023-06-03 16:16:13 +0200

    ui edits

--- gradio-ui.py
+++ gradio-ui.py
@@ -10,11 +10,14 @@ with gr.Blocks(theme=theme) as ui:
     with gr.Row():
         with gr.Column(scale=1):
             message = gr.Audio(source="microphone", type="filepath")
-            btn1 = gr.Button("Respond")
+    with gr.Row():
+        btn1 = gr.Button("Respond")
+    with gr.Row():
         with gr.Column(scale=1):
             audio_response = gr.Audio()
     with gr.Row():
         text_response = gr.Textbox(label="Transcript", max_lines=10)
+    with gr.Row():
         btn2 = gr.Button("Save Conversation")
 
     btn1.click(fn=d.respond, inputs=message, outputs=[audio_response, text_response])
